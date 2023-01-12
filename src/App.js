3e075eb453450e01ee7689dd7471c6a42be62469
Pham Van Nhat <114339708+minhnhatzc@users.noreply.github.com>
2023-01-13 02:05:22 +0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,85 @@
-import logo from './logo.svg';
-import './App.css';
+import AddCandidate from "./Components/AddCandidate";
+import Voting from "./Components/Voting";
+import { useState, useEffect } from "react";
+import { ConnectButton } from "@rainbow-me/rainbowkit";
+import { useAccount, useContract, useSigner } from 'wagmi'
+import { CONTRACT_ADDRESS, CONTRACT_ABI } from "./CONTRACT";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
 
+	const [screen, setScreen] = useState('home')
+
+	const { address } = useAccount()
+
+
+
+	const { data: signer } = useSigner()
+	const contract = useContract({
+		address: CONTRACT_ADDRESS,
+		abi: CONTRACT_ABI,
+		signerOrProvider: signer
+	})
+
+	console.log(contract)
+
+
+	useEffect(() => {
+		if (contract) {
+			const getCandidateCount = async () => {
+				const count = await contract.candidateCount()
+				console.log("Candidate Count ", count.toString())
+			}
+			const getTotalVotes = async () => {
+				const count = await contract.totalVotes()
+				console.log("Total Votes ", count.toString())
+			}
+			getCandidateCount()
+			getTotalVotes()
+		}
+	}, [contract])
+
+	const RenderScreen = () => {
+		return (
+			<div className="flex flex-col gap-4 items-center justify-center min-h-screen">
+				{
+					screen === 'addCandidate' ? (
+						<AddCandidate setScreen={setScreen} />
+					) : (
+						<Voting setScreen={setScreen} />
+					)
+				}
+			</div>
+		)
+	}
+
+
+	return (
+		<div className="bg-black text-white">
+			<div className="flex items-center justify-between flex-row px-4 py-2">
+				{/* Logo */}
+				<h1 className="text-2xl font-bold">Election</h1>
+				<ConnectButton />
+			</div>
+			{
+				screen === 'home' ? (
+					<div className="flex flex-col gap-4 items-center justify-center min-h-screen">
+						<h1 className="text-4xl font-extrabold">Election</h1>
+						{
+							address ? (
+								<div className="flex flex-row gap-4 items-center justify-center">
+									<button onClick={() => setScreen('addCandidate')} className="bg-blue-500 text-white px-4 py-2 rounded-lg">Add Candidate</button>
+									<button onClick={() => setScreen('vote')} className="bg-blue-500 text-white px-4 py-2 rounded-lg">Vote</button>
+								</div>
+							) : (
+								<ConnectButton />
+							)
+						}
+					</div>
+				) : (
+					<RenderScreen />
+				)
+			}
+		</div>
+	);
+}
 export default App;
---
+++ src/CONTRACT.js
@@ -0,0 +1,131 @@
+CONTRACT_ADDRESS = "0x2f5A56b9916836eeC4Bf74DafCaC55834294E006"
+CONTRACT_ABI = [
+	{
+		"inputs": [
+			{
+				"internalType": "string",
+				"name": "name",
+				"type": "string"
+			},
+			{
+				"internalType": "string",
+				"name": "party",
+				"type": "string"
+			},
+			{
+				"internalType": "string",
+				"name": "imageUri",
+				"type": "string"
+			}
+		],
+		"name": "addCandidate",
+		"outputs": [],
+		"stateMutability": "nonpayable",
+		"type": "function"
+	},
+	{
+		"inputs": [],
+		"stateMutability": "nonpayable",
+		"type": "constructor"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "uint256",
+				"name": "id",
+				"type": "uint256"
+			}
+		],
+		"name": "vote",
+		"outputs": [],
+		"stateMutability": "nonpayable",
+		"type": "function"
+	},
+	{
+		"inputs": [],
+		"name": "candidateCount",
+		"outputs": [
+			{
+				"internalType": "uint256",
+				"name": "",
+				"type": "uint256"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "uint256",
+				"name": "",
+				"type": "uint256"
+			}
+		],
+		"name": "candidates",
+		"outputs": [
+			{
+				"internalType": "string",
+				"name": "name",
+				"type": "string"
+			},
+			{
+				"internalType": "string",
+				"name": "party",
+				"type": "string"
+			},
+			{
+				"internalType": "string",
+				"name": "imageUri",
+				"type": "string"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"inputs": [],
+		"name": "owner",
+		"outputs": [
+			{
+				"internalType": "address",
+				"name": "",
+				"type": "address"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"inputs": [],
+		"name": "totalVotes",
+		"outputs": [
+			{
+				"internalType": "uint256",
+				"name": "",
+				"type": "uint256"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "uint256",
+				"name": "",
+				"type": "uint256"
+			}
+		],
+		"name": "votes",
+		"outputs": [
+			{
+				"internalType": "uint256",
+				"name": "",
+				"type": "uint256"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	}
+]
